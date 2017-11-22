0f299221c5c2301dd7134632f662c3e8e7da2f04
Volodymyr Savchenko <vladimir.savchenko@gmail.com>
2017-11-22 18:09:09 +0100

    tree

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,3 @@
-TD="/home/savchenk/work/osa11/software/ii_context/ii_context"
-cd $TD
-
 export PFILES=$TD:$PFILES
 ./ii_context \
     idxLowThre="$REP_BASE_PROD/scw/1857/rev.001/idx/isgri_context_index.fits[1]" \
